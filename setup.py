efb77e5b599d4c5c57345e0c593b635ccb65551d
Zishan <zishanenator@gmail.com>
2024-07-10 22:43:56 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "us_visa",
+    version = "0.0.0",
+    author = "Zishan",
+    author_email = "zishanenator@gmail.com",
+    packages = find_packages()
+)
